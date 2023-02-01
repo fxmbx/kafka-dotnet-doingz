0f014527f28a94fc2ea0c227ebddabd73df1e67e
fxmbx <funbiolaore@gmail.com>
2023-02-01 02:01:37 +0100

    feat: doingz

---
+++ consumer/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var consumerConfig = new ConsumerConfig
+        {
+            BootstrapServers = "pkc-l6wr6.europe-west2.gcp.confluent.cloud:9092",
+            SecurityProtocol = SecurityProtocol.SaslSsl,
+            SaslMechanism = SaslMechanism.Plain,
+            SaslUsername = "",
+            SaslPassword = "",
+            GroupId = "kafka-dotnet-doingz-consumer-group",
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+
+            EnableAutoOffsetStore = false,
+            EnableAutoCommit = true,
+            StatisticsIntervalMs = 5000,
+            SessionTimeoutMs = 6000,
+            EnablePartitionEof = true,
+            PartitionAssignmentStrategy = PartitionAssignmentStrategy.CooperativeSticky
+        };
+        const string topicName = "email_topic";
+
+        CancellationTokenSource cancellationToken = new();
+        Console.CancelKeyPress += (_, x) =>
+        {
+            x.Cancel = true;
+            cancellationToken.Cancel();
+        };
+
+        using (var consumer = new ConsumerBuilder<Ignore, EmailMessage>(consumerConfig)
+        .SetValueDeserializer(new EmailMessage())
+        .SetLogHandler((_, logHandler) => { System.Console.WriteLine(logHandler.Message); })
+        .SetErrorHandler((_, errorHandler) => { System.Console.WriteLine(errorHandler.Reason); })
+        .Build())
+        {
+            consumer.Subscribe(topicName);
+            try
+            {
+                while (true)
+                {
+                    var result = consumer.Consume(cancellationToken.Token);
+                    System.Console.WriteLine($"Consumes event from topic {topicName} with key {result.TopicPartitionOffset} and value {result.Message.Value}");
+
+                }
+            }
+            catch (OperationCanceledException)
+            {
+
+            }
+            finally
+            {
+                consumer.Close();
+            }
+        }
+
+    }
+}
+class EmailMessage : ISerializer<EmailMessage>, IDeserializer<EmailMessage>
+{
+    public string? Subject { get; set; }
+    public string? Body { get; set; }
+    public string? To { get; set; }
+
+    public EmailMessage Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+    {
+        return JsonSerializer.Deserialize<EmailMessage>(data.ToArray());
+    }
+
+    public byte[] Serialize(EmailMessage data, SerializationContext context)
+    {
+        using var ms = new MemoryStream();
+
+        string jsonString = JsonSerializer.Serialize(data);
+        var writer = new StreamWriter(ms);
+        writer.Write(jsonString);
+        ms.Position = 0;
+        return ms.ToArray();
+    }
+}
---
+++ producer/Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json.Serialization;
+using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const string topicName = "email_topic";
+
+        EmailMessage[] emailMessages = {
+            new EmailMessage{ Subject = "Email Test1", Body = "This is the body for the First email",To="funbiolaore@gmail.com"},
+            new EmailMessage{ Subject = "Email Test2", Body = "This is the body for the Second email",To="fxmbxolxorx@gmail.com"},
+            new EmailMessage{ Subject = "Email Test3", Body = "This is the body for the Third email",To="funbi@morerave.com"},
+            };
+        var producerConfiguratoin = new ProducerConfig
+        {
+            BootstrapServers = "pkc-l6wr6.europe-west2.gcp.confluent.cloud:9092",
+            SecurityProtocol = SecurityProtocol.SaslSsl,
+            SaslMechanism = SaslMechanism.Plain,
+            SaslUsername = "",
+            SaslPassword = ""
+        };
+
+        using var producer = new ProducerBuilder<string, EmailMessage>(producerConfiguratoin)
+        .SetValueSerializer(new EmailMessage())
+        .Build();
+        var numProduced = 0;
+        Random rnd = new();
+        const int numMessages = 10;
+        for (int i = 0; i < numMessages; ++i)
+        {
+            var emailMessage = emailMessages[rnd.Next(emailMessages.Length)];
+
+            producer.Produce(topicName, new Message<string, EmailMessage> { Key = emailMessage.To!, Value = emailMessage },
+
+            (deliveryReport) =>
+            {
+                if (deliveryReport.Error.Code != ErrorCode.NoError)
+                {
+                    Console.WriteLine($"Failed to deliver Message : {deliveryReport.Error.Reason}");
+                }
+                else
+                {
+                    Console.WriteLine($"Prodiced event to topic {topicName}: key = {emailMessage.To,-10} value = {emailMessage}");
+                    numProduced += 1;
+                }
+            });
+        }
+
+        producer.Flush(TimeSpan.FromSeconds(10));
+        Console.WriteLine($"{numProduced} messages were produced to topic {topicName}");
+    }
+}
+
+class EmailMessage : ISerializer<EmailMessage>, IDeserializer<EmailMessage>
+{
+    public string? Subject { get; set; }
+    public string? Body { get; set; }
+    public string? To { get; set; }
+
+    public EmailMessage Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+    {
+        return JsonSerializer.Deserialize<EmailMessage>(data.ToArray());
+    }
+
+    public byte[] Serialize(EmailMessage data, SerializationContext context)
+    {
+        using var ms = new MemoryStream();
+
+        string jsonString = JsonSerializer.Serialize(data);
+        var writer = new StreamWriter(ms);
+        writer.Write(jsonString);
+        ms.Position = 0;
+        return ms.ToArray();
+    }
+}
